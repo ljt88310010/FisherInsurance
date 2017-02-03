d4fe016f775689bfc57bb335823ab8da5d8883c9
Jiatong Li <Jiatong Li>
2017-02-02 22:18:07 -0500

    Exercise 3 finished

--- wwwroot/Controllers/Customercarecontroller.cs
+++ wwwroot/Controllers/Customercarecontroller.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+
 public class CustomercareController : Controller
 { 
+ [Route("customer")]
+ [Route("customer/Index")]
  public ActionResult Index()
  {
  return Ok("This is the index of the Customercare");
  }
-  [Route("Customercare/Claims")]
+  [Route("customer/Claims")]
  public ActionResult Quote()
  {
       return Ok("This is the Quote of the Customercare");
+ }
+ [Route("customer/claim/fileclaim")]
+ [Route("customer/fileclaim")]
+ public ActionResult NewClaim()
+ {
+      return Ok("This is the NewClaim of the Customercare");
+ }
+ [Route("customer/claimstatus")]
+ [Route("customer/claim/myclaims")]
+ public ActionResult ClaimHistory()
+ {
+      return Ok("This is the ClaimHistory of the Customercare");
  }
 }
