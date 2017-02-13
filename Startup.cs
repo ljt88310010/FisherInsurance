ae89868b866728b99e808e0739cc6a8642c45068
Jiatong Li <Jiatong Li>
2017-02-13 18:47:33 -0500

    2/13 in class

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -22,12 +17,8 @@ namespace FisherInsurance
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-           app.UseMvc(routes =>
-           {
-                routes.MapRoute(
-                name: "default",
-                template: "{controller=Home}/{action=Index}/{id?}");
-           });
+           app.UseStaticFiles();
+           app.UseMvcWithDefaultRoute();
         }
     }
 }
